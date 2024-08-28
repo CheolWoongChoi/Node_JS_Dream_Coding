84c2fd8e3e00848041293c594df72e48110113a3
CheolWoong Choi <bluesky4381@naver.com>
2024-08-28 13:23:52 +0900

    refactor : 클라이언트 코드 리팩토링

--- dwitter/client/src/service/tweet.js
+++ dwitter/client/src/service/tweet.js
@@ -1,59 +1,32 @@
 export default class TweetService {
-  constructor(baseURL) {
-    this.baseURL = baseURL;
+  constructor(http) {
+    this.http = http;
   }
 
   async getTweets(username) {
     const query = username ? `?username=${username}` : "";
-    const response = await fetch(`${this.baseURL}/dweets${query}`, {
+    return this.http.fetch(`/dweets${query}`, {
       method: "GET",
-      headers: { "Content-Type": "application/json" },
     });
-    const data = await response.json();
-
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-
-    return data;
   }
 
   async postTweet(text) {
-    const response = await fetch(`${this.baseURL}/dweets`, {
+    return this.http.fetch(`/dweets`, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text, username: "ellie", name: "Ellie" }),
     });
-    const data = await response.json();
-
-    if (response.status !== 201) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async deleteTweet(tweetId) {
-    const response = await fetch(`${this.baseURL}/dweets/${tweetId}`, {
+    return this.http.fetch(`/dweets/${tweetId}`, {
       method: "DELETE",
-      headers: { "Content-Type": "application/json" },
     });
-    if (response.status !== 204) {
-      throw new Error();
-    }
   }
 
   async updateTweet(tweetId, text) {
-    const response = await fetch(`${this.baseURL}/dweets/${tweetId}`, {
+    return this.http.fetch(`/dweets/${tweetId}`, {
       method: "PUT",
-      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text }),
     });
-    const data = await response.json();
-
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-
-    return data;
   }
 }
