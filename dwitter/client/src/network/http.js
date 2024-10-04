9f09a5512fd23c6c6d38658a516bbcb06855c177
CheolWoong Choi <bluesky4381@naver.com>
2024-10-04 21:48:27 +0900

    서버지키기 : 프론트쪽 http 모듈을 axios로 대체

--- dwitter/client/src/network/http.js
+++ dwitter/client/src/network/http.js
@@ -1,41 +1,39 @@
+import axios from "axios";
 export default class HttpClient {
   constructor(baseURL, authErrorEventBus, getCsrfToken) {
-    this.baseURL = baseURL;
     this.authErrorEventBus = authErrorEventBus;
     this.getCsrfToken = getCsrfToken;
+    this.client = axios.create({
+      baseURL,
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    });
   }
 
   async fetch(url, options) {
-    const res = await fetch(`${this.baseURL}${url}`, {
-      ...options,
+    const { body, method, headers } = options;
+    const req = {
+      url,
+      method,
       headers: {
-        "Content-Type": "application/json",
-        ...options.headers,
+        ...headers,
         "dwitter-csrf-token": this.getCsrfToken(),
       },
-      credentials: "include",
-    });
+      data: body,
+      withCredentials: true,
+    };
 
-    let data;
     try {
-      data = await res.json();
-    } catch (e) {
-      console.error(e);
-    }
-
-    if (res.status > 299 || res.status < 200) {
-      const message =
-        data && data.message ? data.message : "Something went wrong!";
-      const error = new Error(message);
-
-      if (res.status === 401) {
-        this.authErrorEventBus.notify(error);
-        return;
+      const res = await this.client(req);
+      return res.data;
+    } catch (err) {
+      if (err.response) {
+        const data = err.response.data;
+        const message =
+          data && data.message ? data.message : "Something went wrong! 😅";
+        throw new Error(message);
       }
-
-      throw error;
+      throw new Error("Connection Error ❌");
     }
-
-    return data;
   }
 }
